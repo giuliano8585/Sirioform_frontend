f07d1ee76a8ae9d6dfd64ee69c947945bd7a40fc
IjazkhanAfridi <iafridi640@gmail.com>
2024-12-31 21:31:38 +0500

    add latest changes

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { useNavigate } from 'react-router-dom';
 
 function Home() {
+  const navigate = useNavigate()
   return (
     <div className="container">
       <div className="jumbotron mt-4">
@@ -9,8 +11,9 @@ function Home() {
         <p className="lead">This is a simple product ordering system built with React and Node.js.</p>
         <hr className="my-4" />
         <p>Login to start managing your orders and products.</p>
-        <a className="btn btn-primary btn-lg" href="/login" role="button">Login</a>
-        <a className="btn btn-success btn-lg ml-3" href="/register" role="button">Register</a>
+        <button onClick={()=>navigate('/login')} className="btn btn-primary btn-lg" href="" role="button">Login</button>
+        <button onClick={()=>navigate('/register-center')} className="btn btn-success btn-lg ml-3" role="button">Register Center</button>
+        <button onClick={()=>navigate('/register-instructor')} className="btn btn-success btn-lg ml-3" role="button">Register Instructor</button>
       </div>
     </div>
   );
